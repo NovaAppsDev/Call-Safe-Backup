49334f91d9b97a515fcbf99a75f3b5bdc0db4402
omid <xdvdxzv@gmail.com>
2025-01-17 07:24:42 -0800

    change design - #12

--- app/src/main/java/ir/novaapps/callsafebackup/view/HistoryFragment.kt
+++ app/src/main/java/ir/novaapps/callsafebackup/view/HistoryFragment.kt
@@ -1,26 +1,18 @@
 package ir.novaapps.callsafebackup.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
-import ir.novaapps.callsafebackup.databinding.HomeFragmentBinding
-import ir.novaapps.callsafebackup.databinding.ProfileFragmentBinding
+import ir.novaapps.callsafebackup.databinding.HistoryFragmentBinding
 import ir.novaapps.callsafebackup.utils.BaseFragment
-import ir.novaapps.callsafebackup.viewmodel.MainViewModel
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class ProfileFragment : BaseFragment<ProfileFragmentBinding>() {
+class HistoryFragment : BaseFragment<HistoryFragmentBinding>() {
 
-    override val bindingInflater: (inflater: LayoutInflater) -> ProfileFragmentBinding
-        get() = ProfileFragmentBinding::inflate
+    override val bindingInflater: (inflater: LayoutInflater) -> HistoryFragmentBinding
+        get() = HistoryFragmentBinding::inflate
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
