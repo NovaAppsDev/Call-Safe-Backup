73fe0fac8da6ed5f3d3fb58c0b2526139231464c
omid <xdvdxzv@gmail.com>
2025-02-09 09:54:17 -0800

    complete theme dark and light - #12

--- app/src/main/java/ir/novaapps/callsafebackup/MainActivity.kt
+++ app/src/main/java/ir/novaapps/callsafebackup/MainActivity.kt
@@ -1,8 +1,10 @@
 package ir.novaapps.callsafebackup
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
@@ -10,6 +12,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ir.novaapps.callsafebackup.databinding.ActivityMainBinding
+import ir.novaapps.callsafebackup.utils.SharedPreferences
 
 
 @AndroidEntryPoint
@@ -18,12 +21,13 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = requireNotNull(_binding)
     private lateinit var navHostFragment: NavHostFragment
-
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
-
+        sharedPreferences = SharedPreferences(this)
+        val darkTheme = sharedPreferences.getDetailBoolean("statusTheme", false)
         navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
 
@@ -49,6 +53,17 @@ class MainActivity : AppCompatActivity() {
         binding.navigationBottom.setupWithNavController(navHostFragment.navController)
 
         setContentView(binding.root)
+
+        if (!darkTheme) {
+            setThemeMode(AppCompatDelegate.MODE_NIGHT_NO)
+        } else {
+            setThemeMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        binding.btnChangeTheme.setOnClickListener {
+            toggleTheme()
+        }
+
     }
 
     override fun onNavigateUp(): Boolean {
@@ -59,5 +74,20 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         _binding = null
     }
+
+    fun AppCompatActivity.setThemeMode(themeMode: Int) {
+        AppCompatDelegate.setDefaultNightMode(themeMode)
+        delegate.applyDayNight()
+    }
+
+    private fun toggleTheme() {
+        val isDarkTheme = sharedPreferences.getDetailBoolean("statusTheme", false)
+        sharedPreferences.setDetailBoolean("statusTheme", !isDarkTheme)
+        AppCompatDelegate.setDefaultNightMode(
+            if (isDarkTheme) AppCompatDelegate.MODE_NIGHT_NO else AppCompatDelegate.MODE_NIGHT_YES
+        )
+        recreate()
+    }
+
 
 }
